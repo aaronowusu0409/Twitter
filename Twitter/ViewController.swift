36c63d0b8c1e64e604fdbe60677555bf57eed3bc
Aaron Owusu <Aaron@Jesses-MacBook-Pro.local>
2017-10-08 18:01:36 +0100

    Initial Commit

---
+++ Twitter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Twitter
+//
+//  Created by Aaron Owusu on 08/10/2017.
+//  Copyright © 2017 Aaron Owusu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
